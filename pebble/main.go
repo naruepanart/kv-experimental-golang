26c2946f646192950f27fe621fc21e5797b2180f
Naruepanart Siangsanan <naruepanart1201@gmail.com>
2024-10-25 01:22:36 +0700

    Update main.go

--- pebble/main.go
+++ pebble/main.go
@@ -10,14 +10,17 @@ func main() {
 	db, _ := pebble.Open("./pebble/data", &pebble.Options{})
 
 	const count = 100000
+	batch := db.NewBatch()
 	for i := 0; i < count; i++ {
-		db.Set([]byte(fmt.Sprintf("key:%d", i)), []byte(fmt.Sprintf("value:%d", i)), pebble.NoSync)
+		batch.Set([]byte(fmt.Sprintf("key:%d", i)), []byte(fmt.Sprintf("value:%d", i)), pebble.NoSync)
 	}
+	batch.Commit(pebble.NoSync)
 
-	for i := 0; i < count; i++ {
-		value, closer, _ := db.Get([]byte(fmt.Sprintf("key:%d", i)))
-		fmt.Println(string(value))
-		defer closer.Close()
-	}
 	db.Close()
 }
+
+// for i := 0; i < count; i++ {
+// 	value, closer, _ := db.Get([]byte(fmt.Sprintf("key:%d", i)))
+// 	fmt.Println(string(value))
+// 	defer closer.Close()
+// }
