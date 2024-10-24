f340277845f4257bc98d7c490c4908fce1603081
Naruepanart Siangsanan <naruepanart1201@gmail.com>
2024-10-25 01:13:27 +0700

    *

--- badger/main.go
+++ badger/main.go
@@ -7,22 +7,26 @@ import (
 )
 
 func main() {
-	db, _ := badger.Open(badger.DefaultOptions("./badger/data"))
+	opt := badger.DefaultOptions("./badger/data")
+	opt.NumVersionsToKeep = 1
+	opt.SyncWrites = false
+	db, _ := badger.Open(opt)
 	defer db.Close()
 
 	const count = 100000
+	wb := db.NewWriteBatch()
+	defer wb.Cancel()
 	for i := 0; i < count; i++ {
-		db.Update(func(txn *badger.Txn) error {
-			return txn.Set([]byte(fmt.Sprintf("key:%d", i)), []byte(fmt.Sprintf("value:%d", i)))
-		})
+		wb.Set([]byte(fmt.Sprintf("key:%d", i)), []byte(fmt.Sprintf("value:%d", i)))
 	}
-
-	for i := 0; i < count; i++ {
-		db.View(func(txn *badger.Txn) error {
-			item, _ := txn.Get([]byte(fmt.Sprintf("key:%d", i)))
-			value, _ := item.ValueCopy(nil)
-			fmt.Println(string(value))
-			return nil
-		})
-	}
+	wb.Flush()
 }
+
+// for i := 0; i < count; i++ {
+// 	db.View(func(txn *badger.Txn) error {
+// 		item, _ := txn.Get([]byte(fmt.Sprintf("key:%d", i)))
+// 		value, _ := item.ValueCopy(nil)
+// 		fmt.Println(string(value))
+// 		return nil
+// 	})
+// }
--- bbolt/main.go
+++ bbolt/main.go
@@ -9,42 +9,41 @@ import (
 )
 
 func main() {
-	db, err := bolt.Open("./bbolt/data.db", 0600, nil)
+	db, err := bolt.Open("./data.db", 0600, nil)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer db.Close()
 
 	const count = 100000
-	db.Update(func(tx *bolt.Tx) error {
+	db.Batch(func(tx *bolt.Tx) error {
 		bucket, err := tx.CreateBucketIfNotExists([]byte("mybucket"))
 		if err != nil {
 			return err
 		}
 		for i := 0; i < count; i++ {
 			key := fmt.Sprintf("key:%d", i)
-			err = bucket.Put([]byte(key), []byte(fmt.Sprintf("value:%d", i)))
-			if err != nil {
+			if err := bucket.Put([]byte(key), []byte(fmt.Sprintf("value:%d", i))); err != nil {
 				return err
 			}
 		}
 		return nil
 	})
-
-	db.View(func(tx *bolt.Tx) error {
-		bucket := tx.Bucket([]byte("mybucket"))
-		if bucket == nil {
-			return fmt.Errorf("bucket not found")
-		}
-
-		for i := 0; i < count; i++ {
-			key := fmt.Sprintf("key:%d", i)
-			value := bucket.Get([]byte(key))
-			if value == nil {
-				return fmt.Errorf("key %s not found", key)
-			}
-			fmt.Println(string(value))
-		}
-		return nil
-	})
 }
+
+// db.View(func(tx *bolt.Tx) error {
+// 	bucket := tx.Bucket([]byte("mybucket"))
+// 	if bucket == nil {
+// 		return fmt.Errorf("bucket not found")
+// 	}
+
+// 	for i := 0; i < count; i++ {
+// 		key := fmt.Sprintf("key:%d", i)
+// 		value := bucket.Get([]byte(key))
+// 		if value == nil {
+// 			return fmt.Errorf("key %s not found", key)
+// 		}
+// 		fmt.Println(string(value))
+// 	}
+// 	return nil
+// })
